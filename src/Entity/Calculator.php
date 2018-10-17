6dc4c380f9bf195ad5faaeee12747029393b4f4e
Louis HARANG <lharang.pro@gmail.com>
2018-10-17 16:08:08 +0200

    Bootstrap phpstan, phpunit, husky & php-cs-fixer

---
+++ src/Entity/Calculator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Entity;
+
+class Calculator
+{
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+}
---
+++ tests/CalculatorTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+use App\Entity\Calculator;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    public function testAdd()
+    {
+        $calculator = new Calculator();
+        $result = $calculator->add(30, 12);
+
+        // assert that your calculator added the numbers correctly!
+        $this->assertEquals(42, $result);
+    }
+}
