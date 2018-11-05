2eab111868b625c10a5cc784f664de1dcd5b7162
Louis Harang <vundaboy@gmail.com>
2018-11-05 10:50:26 +0100

    Adding random readme to project (#4)

---
+++ public/build/js/app.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["js/app"],{
+
+/***/ "./assets/js/app.js":
+/*!**************************!*\
+  !*** ./assets/js/app.js ***!
+  \**************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("\n\n//# sourceURL=webpack:///./assets/js/app.js?");
+
+/***/ })
+
+},[["./assets/js/app.js","runtime"]]]);
