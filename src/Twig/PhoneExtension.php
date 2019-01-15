7b21382f10d5c3f5d7fd6dae7017c2dfd90d6f31
ThisIsJuke <liege.arthur60@gmail.com>
2019-01-15 19:47:49 +0100

    Feature/separate front back (#53)
    * CD test (#7)
    
    * Adding random readme to project (#4)
    
    * Adding docker to project (#5)
    
    Changed to develop
    
    * Bootstrap/postgres (#6)
    
    * Fixing DB
    
    * Changing to postgre
    
    * CI testing
    
    * CI test #2
    
    * Adding random readme to project (#4)
    
    * Adding docker to project (#5)
    
    Changed to develop
    
    * Bootstrap/postgres (#6)
    
    * Fixing DB
    
    * Changing to postgre
    
    * Add webpack / separate front back / init twig templates
    
    * Add login + users management + roles (#51)
    
    * Add webpack / separate front back / init twig templates

---
+++ src/Twig/PhoneExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class PhoneExtension extends AbstractExtension{
+    public function getFilters(){
+        return [
+            new TwigFilter('format_phone', [$this, 'formatPhone'])
+        ];
+    }
+
+    public function formatPhone(string $phone){
+        return preg_replace('/(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})/','\1 \2 \3 \4 \5', $phone);
+    }
+}
