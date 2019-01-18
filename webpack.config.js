8244a4c98143403ae8a39cd7d7f481977cf584d9
ThisIsJuke <liege.arthur60@gmail.com>
2019-01-18 14:39:30 +0100

    Tailwind config (#76)

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,14 @@ var Encore = require('@symfony/webpack-encore');
 Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
-    .addEntry('app', './assets/js/app.js')
+    .addEntry('js/app', './assets/js/app.js') // your js entry file
+    .addStyleEntry('css/app', './assets/scss/app.scss') // your less/scss entry file
     .enableSingleRuntimeChunk()
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction())
-    .enableSassLoader()
+    .enablePostCssLoader()
+    .enableLessLoader()
+    .enableSassLoader(function (options) {}, {
+        resolveUrlLoader: false
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
