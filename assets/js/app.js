1162dffa41257553b9c1039299dd3a3ffabcdcd7
Liege Arthur <aliege.ext@wedigital.garden>
2019-02-04 14:09:13 +0100

    WIP : bootstrap

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,10 @@
+require('../scss/app.scss');
 const $ = require('jquery');
-require('popper.js');
 require('bootstrap');
+
+
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+});
