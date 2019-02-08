71feade0426f08d66427731e2efaba0e793daf72
Louis Harang <vundaboy@gmail.com>
2019-02-08 16:10:31 +0100

    Information crud

---
+++ src/Form/InformationType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Information;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InformationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('publishedAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Information::class,
+        ]);
+    }
+}
