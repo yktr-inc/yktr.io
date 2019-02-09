e1890d49ad3fff5cee7f6b16ebc00fb8f44b3e3d
Louis Harang <vundaboy@gmail.com>
2019-02-09 14:45:18 +0100

    Adding fixtures and last exams data for dashboard (#106)

---
+++ src/DataFixtures/ClassroomFixture.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Classroom;
+use App\Repository\PromotionRepository;
+
+class ClassroomFixture extends Fixture
+{
+
+    public function __construct(PromotionRepository $promotionRepository){
+      $this->promotionRepository = $promotionRepository;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $promotion = $this->promotionRepository->findOneBy(['title'=>'First year']);
+
+        $classroom = new Classroom();
+
+        $classroom->setName('Classroom 1');
+        $classroom->setPromotion($promotion);
+        $manager->persist($classroom);
+
+        $manager->flush();
+    }
+}
