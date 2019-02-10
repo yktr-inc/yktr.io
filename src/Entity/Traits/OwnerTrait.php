b80b6f4b36bb7b28ddcdae788af06f0c4fc3eb8d
Louis Harang <vundaboy@gmail.com>
2019-02-10 18:08:38 +0100

    Adding owner trait & listener on information

---
+++ src/Entity/Traits/OwnerTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use App\Entity\User;
+
+
+trait OwnerTrait
+{
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $owner;
+
+
+    public function getOwner()
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner)
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+}
